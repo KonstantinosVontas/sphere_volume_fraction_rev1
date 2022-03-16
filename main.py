09569d442846effc13a04d0d8ab19c03acd9a74f
Konstantinos Vontas <99440741+KonstantinosVontas@users.noreply.github.com>
2022-03-16 14:44:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-
+from asyncore import write
+from cgi import test
 from random import random
 import overlap
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-from sklearn.neighbors import radius_neighbors_graph
-
-np.random.seed(1)
-NUMBER_OF_CUBES = 26
 NUMBER_OF_SAMPLES = 10000
 
-SPHERE_MAXIMUM_RADIUS = 10
-SPHERE_MAXIMUM_CENTER_VALUE = 5
+
+def random_normal_generation():
+    v = np.random.normal(size=(1, 3))
+    v = v / np.sqrt(np.sum(v ** 2))
+
+    return v
 
 
-def random_points_generator(number_of_points):
-    maximum_coordinate_value = SPHERE_MAXIMUM_CENTER_VALUE
+def generate_random_point_in_center_cell():
+    maximum_coordinate_value = 1
 
-    points = (np.random.rand(number_of_points, 3) * (2 * maximum_coordinate_value)) - maximum_coordinate_value
+    points = (np.random.rand(1, 3) * (2 * maximum_coordinate_value)) - maximum_coordinate_value
 
     return points
 
 
+def generate_random_sphere():
+    normal = random_normal_generation()
+    radius = np.random.uniform(1, 10)
+
+    point = generate_random_point_in_center_cell()
+
+    center = point - radius * normal
+
+    return overlap.Sphere(tuple(center[0]), radius)
+
+
 def generate_cube_from_center(point):
-    # in order for the cube to be unit
+
     h = 2
 
     cube_vertices = np.array((
@@ -39,69 +53,49 @@ def generate_cube_from_center(point):
     return cube_vertices
 
 
-def generate_sphere_normal_vector():
-    # Φ(θ,ϕ)=(Rsinϕcosθ,Rsinϕsinθ,Rcosϕ)
-    r = 1
-    theta = np.random.uniform(low=0, high=2 * np.pi)
-    phi = np.random.uniform(low=0, high=np.pi)
+def points_of_regular_grid_generation():
+    axis_values = [-2, 0, 2]
 
-    return np.array([r * np.cos(theta) * np.sin(phi),
-                     r * np.sin(theta) * np.sin(phi),
-                     r * np.cos(phi)])
+    points = np.array([0, 0, 0])
 
+    for x in axis_values:
+        for y in axis_values:
+            for z in axis_values:
+                points = np.vstack((points, np.array([x, y, z])))
 
-def find_relative_cubicle_overlap(sphere_overlap):
-    cubicle_volume = 1
-    #cubicle_volume = np.random.rand(0, 1)
-
-    volume_fraction = sphere_overlap / cubicle_volume
-
-    if volume_fraction > 1:
-        volume_fraction = 1
-
-    return volume_fraction
+    return points[1:, :]
 
 
-def generate_random_sphere():
-    radius = np.random.uniform(low=1, high=10)
+def find_relative_cubicle_overlap(cubicle_overlap):
+    maximum_volume = 8
 
-    normal = generate_sphere_normal_vector()
-    sphere_surface_point = (np.random.rand(1, 3) * (2 * 1)) - 1
+    relative_cubicle_overlap = cubicle_overlap / maximum_volume
 
-    center = sphere_surface_point - normal * radius
+    if relative_cubicle_overlap > 1:
+        relative_cubicle_overlap = 1
 
-    return overlap.Sphere(tuple([center[0][0],
-                                 center[0][1],
-                                 center[0][2]]), radius)
+    return relative_cubicle_overlap
 
 
-if __name__ == "__main__":
-    hexahedra = np.zeros(shape=(NUMBER_OF_CUBES + 1, 8, 3))
+points = points_of_regular_grid_generation()
 
-    center_point_of_unit_cirle = np.array([0, 0, 0])
-    hexahedra[0, :, :] = generate_cube_from_center(center_point_of_unit_cirle)
+hexahedra = np.zeros(shape=(points.shape[0], 8, 3))
 
-    random_cube_centers = random_points_generator(NUMBER_OF_CUBES)
+for hexahedron_index in range(hexahedra.shape[0]):
+    hexahedra[hexahedron_index, :, :] = generate_cube_from_center(points[hexahedron_index, :])
 
-    for cube_center_index, cube_center in enumerate(random_cube_centers):
-        hexahedra[cube_center_index + 1, :, :] = generate_cube_from_center(cube_center)
+with open('overlap_curvature.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
 
-    with open('random_cubicle_and_curvatures.csv', 'w') as csv_file:
-        writer = csv.writer(csv_file)
+    for sample_num in range(int(NUMBER_OF_SAMPLES)):
+        sphere = generate_random_sphere()
+        curvature = 2 / sphere.radius
 
-        for test_no in range(int(NUMBER_OF_SAMPLES)):
-            u = []
+        row = []
 
-            sphere = generate_random_sphere()
+        for hexahedron in hexahedra:
+            row.append(find_relative_cubicle_overlap(overlap.overlap(sphere, overlap.Hexahedron(hexahedron))))
 
-            for hexahedron in hexahedra:
-                hex = overlap.Hexahedron(hexahedron)
+        row.append(curvature)
 
-                u.append(find_relative_cubicle_overlap(overlap.overlap(sphere, hex)))
-
-            row = [u_n for u_n in u]
-
-            curvature = 2 / sphere.radius
-            row.append(curvature)
-
-            writer.writerow(row)
+        writer.writerow(row)
